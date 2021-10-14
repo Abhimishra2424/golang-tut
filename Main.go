4fa8e72f9c0ea9e46a72905cd7934f058a4cbf3d
abhishek <abmishra700@gmail.com>
2021-10-14 13:59:04 +0530

    complex types

--- Main.go
+++ Main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	i := 3.14
-	j := 1.7e12
-	k := 2.3e12
+	var i complex128 = 1 + 2i
 	fmt.Printf("%v %T", i, i)
-	fmt.Printf("%v %T", j, j)
-	fmt.Printf("%v %T", k, k)
+
 }
