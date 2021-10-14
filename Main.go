d3150a504500651df9a5c0083f1fd0e05d975df0
abhishek <abmishra700@gmail.com>
2021-10-14 15:37:04 +0530

    array inside array 3matrix'

--- Main.go
+++ Main.go
@@ -4,16 +4,10 @@ import "fmt"
 
 func main() {
 
-	a := [...]int{1, 2, 3, 4, 5, 6, 7, 8}
-	b := a[:]
-	c := a[2:]
-	d := a[:5]
-	e := a[2:7]
-
-	fmt.Printf("B: %v\n", a)
-	fmt.Printf("A: %v\n", b)
-	fmt.Printf("C: %v\n", c)
-	fmt.Printf("D: %v\n", d)
-	fmt.Printf("E: %v\n", e)
-
+	var identityMatrix [3][3]int = [3][3]int{
+		{1, 0, 0},
+		{0, 1, 1},
+		{0, 0, 1},
+	}
+	fmt.Println(identityMatrix)
 }
