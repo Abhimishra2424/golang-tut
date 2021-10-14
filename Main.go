e12584df5ff7b44d6302967b52c57c8a0470a38a
abhishek <abmishra700@gmail.com>
2021-10-14 16:05:39 +0530

    check karte hai ki nahi hai

--- Main.go
+++ Main.go
@@ -6,14 +6,16 @@ import (
 
 func main() {
 
-	// shopingCard := make(map[strings]int)
-
 	shopingCard := map[string]int{
 		"keyboard": 100,
 		"mouse":    50,
 	}
 	shopingCard["keyboard"] = 200
 	shopingCard["Monitor"] = 600
+	_, ok := shopingCard["phone"]
+	_, hai := shopingCard["mouse"]
 
 	fmt.Println(shopingCard)
+	fmt.Println(ok)
+	fmt.Println(hai)
 }
