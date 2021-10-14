286f98ca62fe8c07f908f34ef287c3fdc26ffbe0
abhishek <abmishra700@gmail.com>
2021-10-14 15:17:19 +0530

    change element with add new element in array

--- Main.go
+++ Main.go
@@ -5,9 +5,7 @@ import "fmt"
 func main() {
 
 	var amts [3]int = [3]int{10, 20, 30}
-	amt := [...]int{30, 40, 40, 0, 3, 2, 4, 52, 1}
-	fmt.Printf("amounts:%v", amts)
-	fmt.Printf("amounts:%v", amt)
-	fmt.Printf("%v\n", len(amt))
+	amts[0] = 20
+	fmt.Printf("Amouts: %v\n", amts)
 
 }
