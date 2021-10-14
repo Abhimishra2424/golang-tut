b3fa9c785b51d7d21e7b99ace4c785232b5b6d7c
abhishek <abmishra700@gmail.com>
2021-10-14 13:16:10 +0530

    using the conversion from any of the data type to string this is the use of import package

--- Main.go
+++ Main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 
 	var i = 12
 	var j float32 = float32(i)
+	var foo string = strconv.Itoa(i)
 	fmt.Printf("%v, %T\n", i, i)
 	fmt.Printf("%v, %T\n", j, j)
+	fmt.Printf("%v, %T\n", foo, foo)
 }
