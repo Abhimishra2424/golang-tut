e95933e2ce0cc7f34834994a84286d6de3638ad2
abhishek <abmishra700@gmail.com>
2021-10-14 14:30:20 +0530

    const value in all

--- Main.go
+++ Main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
+
+	
 	const i int = 12
+	const j float32 = 3.14
+	const k string = "abhi"
+	const l bool = true
 	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
 
 }
