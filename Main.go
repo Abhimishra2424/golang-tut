766b39105d1b4f4f23f2256a93bc603133a4e88e
abhishek <abmishra700@gmail.com>
2021-10-14 14:38:19 +0530

    const declear types

--- Main.go
+++ Main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+// User Constant
+const (
+	User    string = "admin"
+	Product string = "Product"
+)
+
 func main() {
 
-	
 	const i int = 12
 	const j float32 = 3.14
 	const k string = "abhi"
@@ -13,5 +18,8 @@ func main() {
 	fmt.Println(j)
 	fmt.Println(k)
 	fmt.Println(l)
+
+	var a int = 13
+	fmt.Println(i + a)
 
 }
