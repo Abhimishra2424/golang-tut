f40213370e4f907be2cd75073fca9741d2a11039
abhishek <abmishra700@gmail.com>
2021-10-14 13:54:44 +0530

    floating

--- Main.go
+++ Main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	i := 20
-	j := 3
-	fmt.Println(i + j)
-	fmt.Println(i - j)
-	fmt.Println(i * j)
-	fmt.Println(i / j)
+	i := 3.14
+	j := 1.7e12
+	k := 2.3e12
+	fmt.Printf("%v %T", i, i)
+	fmt.Printf("%v %T", j, j)
+	fmt.Printf("%v %T", k, k)
 }
