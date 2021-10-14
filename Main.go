757085abc2a27dd0acd66bb6f7ee8e9d805b49f7
abhishek <abmishra700@gmail.com>
2021-10-14 15:10:47 +0530

    with the help of ... we can inc the size of array

--- Main.go
+++ Main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	var amts [3]int = [3]int{10, 20, 30}
-	amt := [3]int{30, 40, 40}
+	amt := [...]int{30, 40, 40, 0, 3, 2, 4, 52, 1}
 	fmt.Printf("amounts:%v", amts)
 	fmt.Printf("amounts:%v", amt)
 	fmt.Printf("%v\n", len(amt))
