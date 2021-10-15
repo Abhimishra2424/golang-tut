eeb5a2279ec8aeb02b83fe0cb03ab3f199209414
abhishek <abmishra700@gmail.com>
2021-10-15 10:57:00 +0530

    this is simple if statement in go

--- Main.go
+++ Main.go
@@ -2,43 +2,10 @@ package main
 
 import "fmt"
 
-type Processor struct {
-	processorName string
-	cores         int
-}
-
-type Memory struct {
-	memoryCapacity int
-	memoryName     string
-}
-
-type Computer struct {
-	Processor
-	Memory
-	price int
-}
-
 func main() {
 
-	c := Computer{}
-	c.price = 5000
-	c.processorName = "intel i5"
-	c.cores = 5
-	c.memoryCapacity = 8
-	c.memoryName = "DDR4"
-
-	c1 := Computer{
-		Processor: Processor{
-			processorName: "intel i 5",
-			cores:         8,
-		},
-		Memory: Memory{
-			memoryCapacity: 15,
-			memoryName:     "DD3",
-		},
+	if true {
+		fmt.Println("this is simple if statement")
 	}
-
-	fmt.Println(c)
-	fmt.Println(c1)
 
 }
