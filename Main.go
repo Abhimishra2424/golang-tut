f64312d7c27385c34fecb1fac3a35aa57c6f10f0
abhishek <abmishra700@gmail.com>
2021-10-15 11:08:45 +0530

    else block add

--- Main.go
+++ Main.go
@@ -10,16 +10,22 @@ func main() {
 	// 	fmt.Println("this is simple if statement")
 	// }
 
-	shopingCart := make(map[string]int)
-	shopingCart = map[string]int{
-		"keyboard": 100,
-		"Mouse":    89,
-	}
+	// shopingCart := make(map[string]int)
+	// shopingCart = map[string]int{
+	// 	"keyboard": 100,
+	// 	"Mouse":    89,
+	// }
 
-	shopingCart["Laptop"] = 1500
+	// shopingCart["Laptop"] = 1500
 
-	if _, ok := shopingCart["Laptop"]; ok {
-		fmt.Println("item exist in the shopCart")
+	// if _, ok := shopingCart["Laptop"]; ok {
+	// 	fmt.Println("item exist in the shopCart")
+	// }
+
+	if i := 2; i == 4 {
+		fmt.Println("this is simple if statement")
+	} else {
+		fmt.Println("this is simple else block")
 	}
 
 }
