5debacfb60594943f33ae5b28611ee451adcd340
abhishek <abmishra700@gmail.com>
2021-10-15 14:43:11 +0530

    nasted loop

--- Main.go
+++ Main.go
@@ -69,7 +69,13 @@ func main() {
 
 	// only one loop for loop in go lang
 
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
 	for i := 0; i < 5; i++ {
-		fmt.Println(i)
+		for j := 0; j < 5; j++ {
+			fmt.Println(i * j)
+		}
 	}
 }
