93a7eebf87b0e2f8437a5dae474f94c8b55746e6
abhishek <abmishra700@gmail.com>
2021-10-15 15:22:18 +0530

    fun with return statement

--- Main.go
+++ Main.go
@@ -5,11 +5,17 @@ import (
 )
 
 func main() {
-
-	wirteMessage("hello user here")
-
+	// msg := "hello here"
+	// wirteMessage(msg)
+	// fmt.Println(msg)
+	value := divide(3, 2)
+	fmt.Print(value)
 }
 
-func wirteMessage(msg string) {
-	fmt.Println(msg)
+// func wirteMessage(msg string) {
+// 	fmt.Println(msg)
+// }
+
+func divide(a float64, b float64) float64 {
+	return a / b
 }
